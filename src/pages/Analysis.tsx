84df436fa1611b3e809923a0e5ead26943869b7d
Jakub Sikora <jakub.sikora@bshg.com>
2022-05-22 22:28:27 +0200

    added export button

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import { ExportButton } from "../components/ExportButton";
+
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import { StatCard } from "../components/StatCard";
 import { SessionHistogram } from "../components/SessionHistogram";
 
 export const AnalysisPage: FC = () => {
+  const handleButtonClick = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  };
+
   return (
     <>
+      <Stack direction="row" justifyContent="space-between" sx={{ mb: 3 }}>
+        <Typography variant="h5">
+          Analiza wyników
+        </Typography>
+        <ExportButton handler={handleButtonClick} />
+      </Stack>
       <Box sx={{ pb: 5 }}>
         <ImageList cols={3} gap={8}>
           <ImageListItem>
@@ -20,11 +34,11 @@ export const AnalysisPage: FC = () => {
             <StatCard label="Max" value={Math.random()} unit="ns" float />
           </ImageListItem>
         </ImageList>
-        
+
         <SessionHistogram />
 
         <ImageList cols={3} gap={8}>
-        <ImageListItem>
+          <ImageListItem>
             <StatCard label="Średnia" value={Math.random()} unit="ns" float />
           </ImageListItem>
           <ImageListItem>

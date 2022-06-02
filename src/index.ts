456f158ddaff09da985f9cb386ae18c727014eef
Jakub Sikora <jakub.sikora@bshg.com>
2022-06-02 19:30:53 +0200

    removed menubar

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+  mainWindow.removeMenu();
 };
 
 // This method will be called when Electron has finished
